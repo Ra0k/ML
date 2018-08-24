eb1d7f6c61a0694c632c9e3a4f5cba9fd823c258
Ra0k <david@szdavid.hu>
2018-08-24 14:45:32 +0200

    extend the algorithm with biases

--- ml/decomposition/NonNegativeMatrixFactorization.py
+++ ml/decomposition/NonNegativeMatrixFactorization.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class MatrixFactorization(object):
     def __init__(self, data, K):
         '''
@@ -11,7 +9,13 @@ class MatrixFactorization(object):
         self.R = np.matrix(data)
         self.D = np.zeros( self.R.shape )
         self.K = K
+           
+        # User and Product biases
+        self.b   = np.nanmean(self.R)
+        self.u_b = np.zeros( self.R.shape[0] )
+        self.p_b = np.zeros( self.R.shape[1] )
         
+        # User and Product matrix
         self.U = np.random.uniform( size=(self.R.shape[0], K) )
         self.P = np.random.uniform( size=(K, self.R.shape[1]) )
     
@@ -30,27 +34,30 @@ class MatrixFactorization(object):
         for _ in range(iterations):
             self._compure_error()
             
-            U = self.U
-            P = self.P
-            
             for i in range(self.R.shape[0]):      
                 for j in range(self.R.shape[1]):
                     for k in range(self.K):
-                        ik = alpha * ( P[k, j] * self.D[i, j] + beta * U[i, k])
-                        kj = alpha * ( U[i, k] * self.D[i, j] + beta * P[k, j])
-                        if np.isfinite(ik):
-                            self.U[i, k] += ik
-                        if np.isfinite(kj):
-                            self.P[k, j] += kj
+                        #update User and Product matrix
+                        U_ik = alpha * ( self.P[k, j] * self.D[i, j] - beta * self.U[i, k])
+                        P_kj = alpha * ( self.U[i, k] * self.D[i, j] - beta * self.P[k, j])
+                        if np.isfinite(U_ik):
+                            self.U[i, k] += U_ik
+                        if np.isfinite(P_kj):
+                            self.P[k, j] += P_kj
+                            
+                        #update User and Product biases
+                        Ub_i = alpha * ( self.D[i, j] - beta * self.u_b[i] )
+                        Pb_j = alpha * ( self.D[i, j] - beta * self.p_b[j] )
+                        if np.isfinite(Ub_i):
+                            self.u_b[i] += Ub_i
+                        if np.isfinite(Pb_j):
+                            self.p_b[j] += Pb_j      
             
-            #non-negativity
-            self.U = self.U.clip(min=0)
-            self.P = self.P.clip(min=0)
             
         return np.nansum(np.nansum(abs(self._compure_error())))
     
     def estimate_all(self):
-        return self.U.dot(self.P)
+        return self.U.dot(self.P) + self.b + self.u_b[:, np.newaxis] + self.p_b[np.newaxis, :]
     
     def estimate(self, x, y):
-        return self.U[x, :].dot(self.P[:, y])
+        return self.U[x, :].dot(self.P[:, y]) + self.b + self.u_b[x] + self.p_b[y]
